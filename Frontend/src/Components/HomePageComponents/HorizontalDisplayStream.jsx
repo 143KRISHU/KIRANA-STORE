920511d99aad94a64c1385654a86dc2337069388
143KRISHU <1712rishabhofficial1997@gmail.com>
2024-09-27 03:09:31 +0530

    the auto complete feature is implemented in Frontend

--- Frontend/src/Components/HomePageComponents/HorizontalDisplayStream.jsx
+++ Frontend/src/Components/HomePageComponents/HorizontalDisplayStream.jsx
@@ -6,137 +6,137 @@ import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
 
 function HorizontalDisplayStream({ subcategory, heading }) {
-      const [subCategoryWiseProduct, setSubCategoryWiseProduct] = useState([])
-      const [horizontalSlideCount, setHorizontalSlideCount] = useState(0)
-      const [isLoading, setIsLoading] = useState(false)
-      const loadingScreen = new Array(8).fill(null)
-      const [isError, setIsError] = useState(false)
-      const navigator = useNavigate()
+  const [subCategoryWiseProduct, setSubCategoryWiseProduct] = useState([])
+  const [horizontalSlideCount, setHorizontalSlideCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const loadingScreen = new Array(8).fill(null)
+  const [isError, setIsError] = useState(false)
+  const navigator = useNavigate()
 
-      const getProductData = async () => {
-            setIsLoading(true)
-            const backendResponse = await fetch(backendRoutesAPI.homePageAPI.subCategoryWiseProduct.url, {
-                  method: backendRoutesAPI.homePageAPI.subCategoryWiseProduct.method,
-                  headers: {
-                        'content-type': "application/json"
-                  },
-                  body: JSON.stringify({ subcategory: subcategory })
-            })
-            const finalRes = await backendResponse.json()
-            if (finalRes.success) {
-                  setSubCategoryWiseProduct(finalRes.data)
-                  setIsError(false)
-                  setIsLoading(false)
-            }
-            else {
-                  setIsError(true)
-                  setIsLoading(false)
-            }
-      }
-      const formattedCurrency = (number) => {
-            return (
-                  number.toLocaleString('en-US', {
-                        style: 'currency',
-                        currency: 'INR', // Change to your desired currency code
-                  }))
-      }
-      const handleForwardMoveButtonInHorizontalSlide = () => {
-            console.log(horizontalSlideCount)
-            if (horizontalSlideCount !== 3) {
-                  setHorizontalSlideCount(horizontalSlideCount + 1)
-            }
-      }
-      const handleBackwardMoveButtonInHorizontalSlide = () => {
-            console.log(horizontalSlideCount)
-            if (horizontalSlideCount !== 0) {
-                  setHorizontalSlideCount(horizontalSlideCount - 1)
-            }
-      }
-      useEffect(() => {
-            getProductData()
-      }, [])
+  const getProductData = async () => {
+    setIsLoading(true)
+    const backendResponse = await fetch(backendRoutesAPI.homePageAPI.subCategoryWiseProduct.url, {
+      method: backendRoutesAPI.homePageAPI.subCategoryWiseProduct.method,
+      headers: {
+        'content-type': "application/json"
+      },
+      body: JSON.stringify({ subcategory: subcategory })
+    })
+    const finalRes = await backendResponse.json()
+    if (finalRes.success) {
+      setSubCategoryWiseProduct(finalRes.data)
+      setIsError(false)
+      setIsLoading(false)
+    }
+    else {
+      setIsError(true)
+      setIsLoading(false)
+    }
+  }
+  const formattedCurrency = (number) => {
+    return (
+      number.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'INR', // Change to your desired currency code
+      }))
+  }
+  const handleForwardMoveButtonInHorizontalSlide = () => {
+    console.log(horizontalSlideCount)
+    if (horizontalSlideCount !== 3) {
+      setHorizontalSlideCount(horizontalSlideCount + 1)
+    }
+  }
+  const handleBackwardMoveButtonInHorizontalSlide = () => {
+    console.log(horizontalSlideCount)
+    if (horizontalSlideCount !== 0) {
+      setHorizontalSlideCount(horizontalSlideCount - 1)
+    }
+  }
+  useEffect(() => {
+    getProductData()
+  }, [])
 
-      return (
-            <>
-                  {
-                        isError ? null :
-                              <div className='bg-white shadow mt-6'>
-                                    <div className='py-4 relative'>
-                                          <div className=' px-4 mb-3 flex justify-between items-center'>
-                                                <div className='text-2xl font-semibold title select-none'>{heading}</div>
-                                                <div className='text-3xl text-[#006D77] h-fit w-fit cursor-pointer hover:scale-125 transition-all'><IoChevronForwardCircle /></div>
-                                          </div>
-                                          {/* Slide Buttons */}
-                                          {
-                                                isLoading ? null : (
-                                                      subCategoryWiseProduct.length !== 9 ? null : (
-                                                            <div className='z-30 h-fit w-full md:flex justify-between absolute top-[40%]  items-center hidden'>
-                                                                  <div onClick={handleBackwardMoveButtonInHorizontalSlide}
-                                                                        className='text-2xl text-white h-24 w-8 flex justify-center shadow-md cursor-pointer
+  return (
+    <>
+      {
+        isError ? null :
+          <div className='bg-white shadow mt-6'>
+            <div className='py-4 relative'>
+              <div className=' px-4 mb-3 flex justify-between items-center'>
+                <div className='text-2xl font-semibold title select-none'>{heading}</div>
+                <div className='text-3xl text-[#006D77] h-fit w-fit cursor-pointer hover:scale-125 transition-all'><IoChevronForwardCircle /></div>
+              </div>
+              {/* Slide Buttons */}
+              {
+                isLoading ? null : (
+                  subCategoryWiseProduct.length !== 9 ? null : (
+                    <div className='z-30 h-fit w-full md:flex justify-between absolute top-[40%]  items-center hidden'>
+                      <div onClick={handleBackwardMoveButtonInHorizontalSlide}
+                        className='text-2xl text-white h-24 w-8 flex justify-center shadow-md cursor-pointer
                                                                   items-center edgeRound-left bg-[#006D77]'
-                                                                        style={{
-                                                                              visibility: `${horizontalSlideCount === 0 ? `hidden` : `visible`}`
-                                                                        }}
-                                                                  ><IoIosArrowBack /></div>
-                                                                  <div onClick={handleForwardMoveButtonInHorizontalSlide}
-                                                                        className='text-2xl text-white h-24 w-8 flex justify-center  shadow-md cursor-pointer
+                        style={{
+                          visibility: `${horizontalSlideCount === 0 ? `hidden` : `visible`}`
+                        }}
+                      ><IoIosArrowBack /></div>
+                      <div onClick={handleForwardMoveButtonInHorizontalSlide}
+                        className='text-2xl text-white h-24 w-8 flex justify-center  shadow-md cursor-pointer
                                                                   items-center edgeRound-right bg-[#006D77]'
-                                                                        style={{
-                                                                              visibility: `${horizontalSlideCount === 3 ? `hidden` : `visible`}`,
-                                                                              right: '0'
-                                                                        }}
-                                                                  > <IoChevronForward /></div>
-                                                            </div>
-                                                      )
-                                                )
-                                          }
-                                          {/* Products List */}
-                                          <div className='mainProductDivCinatiner relative flex bg-white gap-4 px-4 py-2 overflow-hidden'>
-                                                {
-                                                      isLoading ? (
-                                                            loadingScreen.map((_, index) => {
-                                                                  return (
-                                                                        <div key={index}>
-                                                                              <div className='flex flex-nowrap md:flex-col sm:flex-row  border p-[6px] justify-center items-center rounded-md transition-all group' key={index}>
-                                                                                    <div id='productImage' className='w-full md:h-[220px] md:min-w-[200px] sm:h-[64px] sm:max-w-[64px] cursor-pointer'>
-                                                                                          <img src='' className='h-full w-full object-scale-down cursor-pointer bg-slate-200 animate-pulse' />
-                                                                                    </div>
-                                                                                    <div id='productDetail' className=' ml-2 flex flex-col justify-center items-center p-4 gap-4'>
-                                                                                          <div className='w-48 p-3 animate-pulse bg-slate-200 rounded-full'></div>
-                                                                                          <div className='w-48 p-3 animate-pulse bg-slate-200 rounded-full'></div>
-                                                                                    </div>
-                                                                              </div>
-                                                                        </div>)
-                                                            })
-                                                      ) : (
-                                                            subCategoryWiseProduct.map((product, index) => {
-                                                                  return (
-                                                                        <div key={index}>
-                                                                              <div className='flex flex-nowrap md:flex-col sm:flex-row  border p-[6px] rounded-md transition-all group select-none' onClick={() => { navigator(`/productDetail/${product._id}/view`) }} key={product._id}
-                                                                                    style={{
-                                                                                          transform: `translateX(-${horizontalSlideCount * 100}%)`,
-                                                                                          transitionProperty: 'transform',
-                                                                                    }}
-                                                                              >
-                                                                                    <div id='productImage' className='w-full md:h-[220px] md:min-w-[200px] sm:h-[64px] sm:max-w-[64px] cursor-pointer'>
-                                                                                          <img src={product.productImage[0]} className='h-full w-full object-scale-down hover:scale-110 transition-all cursor-pointer' />
-                                                                                    </div>
-                                                                                    <div id='productDetail' className=' ml-2 flex flex-col justify-center items-center'>
-                                                                                          <div className='md:text-lg sm:text-base w-48 text-center select-none text-clip line-clamp-1  capitalize'>{product.productName}</div>
-                                                                                          <div className='md:text-lg sm:text-base font-semibold select-none'>Just {formattedCurrency(product.productSellingPrice)} </div>
-                                                                                    </div>
-                                                                              </div>
-                                                                        </div>
-                                                                  )
-                                                            })
-                                                      )
-                                                }
-                                          </div>
-                                    </div>
-                              </div>
-                  }
-            </>
-      )
+                        style={{
+                          visibility: `${horizontalSlideCount === 3 ? `hidden` : `visible`}`,
+                          right: '0'
+                        }}
+                      > <IoChevronForward /></div>
+                    </div>
+                  )
+                )
+              }
+              {/* Products List */}
+              <div className='mainProductDivCinatiner relative flex bg-white gap-4 px-4 py-2 overflow-hidden'>
+                {
+                  isLoading ? (
+                    loadingScreen.map((_, index) => {
+                      return (
+                        <div key={index}>
+                          <div className='flex flex-nowrap md:flex-col sm:flex-row  border p-[6px] justify-center items-center rounded-md transition-all group' key={index}>
+                            <div id='productImage' className='w-full md:h-[220px] md:min-w-[200px] sm:h-[64px] sm:max-w-[64px] cursor-pointer'>
+                              <img src='' className='h-full w-full object-scale-down cursor-pointer bg-slate-200 animate-pulse' />
+                            </div>
+                            <div id='productDetail' className=' ml-2 flex flex-col justify-center items-center p-4 gap-4'>
+                              <div className='w-48 p-3 animate-pulse bg-slate-200 rounded-full'></div>
+                              <div className='w-48 p-3 animate-pulse bg-slate-200 rounded-full'></div>
+                            </div>
+                          </div>
+                        </div>)
+                    })
+                  ) : (
+                    subCategoryWiseProduct.map((product, index) => {
+                      return (
+                        <div key={index}>
+                          <div className='flex flex-nowrap md:flex-col sm:flex-row  border p-[6px] rounded-md transition-all group select-none' onClick={() => { navigator(`/productDetail/${product._id}/view`) }} key={product._id}
+                            style={{
+                              transform: `translateX(-${horizontalSlideCount * 100}%)`,
+                              transitionProperty: 'transform',
+                            }}
+                          >
+                            <div id='productImage' className='w-full md:h-[220px] md:min-w-[200px] sm:h-[64px] sm:max-w-[64px] cursor-pointer'>
+                              <img src={product.productImage[0]} className='h-full w-full object-scale-down hover:scale-110 transition-all cursor-pointer' />
+                            </div>
+                            <div id='productDetail' className=' ml-2 flex flex-col justify-center items-center'>
+                              <div className='md:text-lg sm:text-base w-48 text-center select-none text-clip line-clamp-1  capitalize'>{product.productName}</div>
+                              <div className='md:text-lg sm:text-base font-semibold select-none'>Just {formattedCurrency(product.productSellingPrice)} </div>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })
+                  )
+                }
+              </div>
+            </div>
+          </div>
+      }
+    </>
+  )
 }
 
 export default HorizontalDisplayStream
--- Frontend/src/Components/Search/Search.jsx
+++ Frontend/src/Components/Search/Search.jsx
@@ -1,31 +1,100 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import backendRoutesAPI from '../../BackendAPI/API'
 import "./Search.css"
+import SuggestionList from './SuggestionList'
 function Search() {
-      const [inputValue, setinputValue] = useState('')
-      const handleInputChange =(event)=>{
-            const {value} = event.target
-            setinputValue(value)
+  const [inputValue, setinputValue] = useState('')
+  const [searchedResult, setsearchedResult] = useState([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleInputChange = (event) => {
+    const { value } = event.target
+    setinputValue(value)
+  }
+
+  const fetchingData = async () => {
+    setLoading(true)
+    const response = await fetch(backendRoutesAPI.searchProductFromBackend.url + `?q=${inputValue}`)
+    const data = await response.json()
+    if (data.success) {
+      console.log(data.data)
+      if (data.data.length > 0) {
+        setsearchedResult(data.data)
+        setLoading(false)
       }
-      const fetchingData = async()=>{
-            const response = await fetch(backendRoutesAPI.searchProductFromBackend.url +`?q=${inputValue}`)
-            const data = await response.json()
-            console.log('Response',data)
+      else if (data.data) {
+        setsearchedResult([{ productName: 'No Result Found', _id: 0 }])
+        setLoading(false)
       }
-      useEffect(()=>{
-            if(inputValue.length > 2){
-                  fetchingData()
-            }
-      },[inputValue])
-      return (
-            <>
-                  <form className='search'>
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                        <input type='search' value={inputValue.toUpperCase()} className="search-bar " placeholder='Search For Product,Brand and More....' 
-                              onChange={handleInputChange} />
-                  </form>
-            </>
-      )
+      else {
+        setsearchedResult([])
+      }
+    }
+    else {
+      setError(data.message)
+      setLoading(false)
+    }
+  }
+
+  const handleSuggestionClick = (suggestion) => {
+    console.log('entered')
+    setinputValue(suggestion['productName'] !== 'No Result Found' ? suggestion['productName'] : '')
+    setsearchedResult([])
+    if(inputValue!==''){
+      showProductPage(suggestion)    }
+  }
+
+  const showProductPage = (suggestion)=>{
+    navigate(`/productDetail/${suggestion._id}/view`)
+  }
+
+  useEffect(() => {
+    if (inputValue.length > 1) {
+      fetchingData()
+    }
+    else {
+      setsearchedResult([])
+      setLoading(false)
+    }
+  }, [inputValue])
+
+
+  return (
+    <>
+      <form className={`relative search`}
+        style={{
+          borderBottomLeftRadius: searchedResult.length > 0 && '0px',
+          borderBottomRightRadius: searchedResult.length > 0 && '0px',
+        }}
+      >
+        <div>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type='search' value={inputValue.toUpperCase()} className="search-bar "
+            placeholder='Search For Product,Brand and More....'
+            onChange={handleInputChange} />
+        </div>
+        {
+          (searchedResult.length > 0 || loading || error)
+          &&
+          <ul className={`suggestionlist h-[300px] w-[522px] z-10 rounded-lg hidden-scrollbar 
+                        overflow-y-scroll gap-2  py-2 top-[2.1rem] absolute bg-white 
+                          ${searchedResult.length === 1 && 'h-fit'}`}>
+            {loading && <div className='loading ml-9'>Loading.....</div>}
+            {error && <div className='loading'>{error}</div>}
+            <SuggestionList dataKey='productName' highlight={inputValue} suggestions={searchedResult}
+              onSuggestionClick={handleSuggestionClick}
+            />
+          </ul>
+        }
+
+      </form>
+
+
+    </>
+  )
 }
 
 export default Search
